bf943b42e505cdb44286d01b3849c14555162673
donikkin <133128982+donikkin@users.noreply.github.com>
2023-11-27 14:30:57 +0200

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+  document.querySelector('.gallery').addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const { tagName, dataset } = event.target;
+
+  if (tagName === 'IMG') {
+    const { source } = dataset;
+
+    const instance = basicLightbox.create(`<img src="${source}" width="800" height="600">`);
+    instance.show();
+
+    document.addEventListener('keydown', function closeOnEscape(e) {
+      if (e.key === 'Escape') {
+        instance.close();
+        document.removeEventListener('keydown', closeOnEscape);
+      }
+    });
+  }
+});
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+document.addEventListener('DOMContentLoaded', function () {
+    const gallery = document.querySelector('.gallery');
 
+    const lightbox = new SimpleLightbox('.gallery a', {
+    });
+
+  });
 console.log(galleryItems);
