fda11d8208b3849ec74f026fb7308b2657443b40
donikkin <133128982+donikkin@users.noreply.github.com>
2023-12-03 20:46:46 +0200

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,78 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+document.addEventListener('DOMContentLoaded', function () {
+  const gallery = document.createElement('ul');
+  gallery.classList.add('gallery');
 
-  document.querySelector('.gallery').addEventListener('click', function (event) {
-  event.preventDefault();
+  galleryItems.forEach(function (item) {
+    const galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery__item');
 
-  const { tagName, dataset } = event.target;
+    const link = document.createElement('a');
+    link.classList.add('gallery__link');
+    link.href = item.original;
 
-  if (tagName === 'IMG') {
-    const { source } = dataset;
+    const image = document.createElement('img');
+    image.classList.add('gallery__image');
+    image.src = item.preview;
+    image.setAttribute('data-source', item.original);
+    image.alt = item.description;
 
-    const instance = basicLightbox.create(`<img src="${source}" width="800" height="600">`);
+    link.appendChild(image);
+    galleryItem.appendChild(link);
+    gallery.appendChild(galleryItem);
+  });
+
+  document.body.appendChild(gallery);
+
+  const galleryLinks = document.querySelectorAll('.gallery__link');
+
+  function openModal(event) {
+    event.preventDefault();
+    const largeImageURL = event.currentTarget.href;
+    const instance = basicLightbox.create(
+      `<img src="${largeImageURL}" width="800" height="600">`,
+      {
+        onShow: (instance) => {
+          console.log('Lightbox is opened.');
+        },
+        onClose: (instance) => {
+          console.log('Lightbox is closed.');
+        },
+      }
+    );
     instance.show();
 
-    document.addEventListener('keydown', function closeOnEscape(e) {
+    function closeModal() {
+      instance.close();
+      instance.element().removeEventListener('click', closeModal);
+      document.removeEventListener('keydown', closeOnEscape);
+    }
+
+    function closeOnEscape(e) {
       if (e.key === 'Escape') {
-        instance.close();
-        document.removeEventListener('keydown', closeOnEscape);
+        closeModal();
       }
-    });
+    }
+
+    instance.element().addEventListener('click', closeModal);
+    document.addEventListener('keydown', closeOnEscape);
   }
+
+  galleryLinks.forEach(function (link) {
+    link.addEventListener('click', openModal);
+  });
 });
+
 console.log(galleryItems);
+
+
+
+
+
+
+
+
+
+
+
